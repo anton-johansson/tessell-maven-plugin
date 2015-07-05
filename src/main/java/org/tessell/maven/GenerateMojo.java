3c6b311c9217c49ad23a24285d1b93373a33dec7
anton-johansson <antoon.johansson@gmail.com>
2015-07-05 16:49:56 +0200

    Initial code

---
+++ src/main/java/org/tessell/maven/GenerateMojo.java
@@ -0,0 +1,48 @@
+package org.tessell.maven;
+
+import java.io.File;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.tessell.generators.Generator;
+
+/**
+ * Maven Mojo for generating Tessell sources.
+ *
+ * @author Anton Johansson
+ */
+@Mojo(name = "generate", defaultPhase = LifecyclePhase.GENERATE_SOURCES)
+public class GenerateMojo extends AbstractMojo {
+
+  @Parameter(defaultValue = "src/main/", required = true)
+  private File inputDirectory;
+
+  @Parameter(defaultValue = "target/generated-sources/tessell/", required = true)
+  private File outputDirectory;
+
+  @Parameter
+  private String viewsPackageName;
+
+  @Parameter
+  private String resourcesPackageName;
+
+  @Parameter
+  private String[] additionalViewgenFiles;
+
+  @Override
+  public void execute() throws MojoExecutionException, MojoFailureException {
+    try {
+      Generator generator = new Generator(inputDirectory, outputDirectory);
+      generator.setViewsPackageName(viewsPackageName);
+      generator.setResourcesPackageName(resourcesPackageName);
+      generator.setAdditionalViewgenFiles(additionalViewgenFiles);
+      generator.generate();
+    } catch (Exception e) {
+      throw new MojoExecutionException("Could not generate tessell sources", e);
+    }
+  }
+}
