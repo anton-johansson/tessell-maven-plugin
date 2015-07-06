74f57e72b5d7f9885724f8b87a1cf3ab8347634e
Anton Johansson <antoon.johansson@gmail.com>
2015-07-06 18:03:48 +0200

    Add the output folder as a compile source root

--- src/main/java/org/tessell/maven/GenerateMojo.java
+++ src/main/java/org/tessell/maven/GenerateMojo.java
@@ -5,9 +5,11 @@ import java.io.File;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
 import org.tessell.generators.Generator;
 
 /**
@@ -33,8 +35,12 @@ public class GenerateMojo extends AbstractMojo {
   @Parameter
   private String[] additionalViewgenFiles = new String[0];
 
+  @Component
+  private MavenProject project;
+
   @Override
   public void execute() throws MojoExecutionException, MojoFailureException {
+	project.addCompileSourceRoot(outputDirectory.getAbsolutePath());
     try {
       Generator generator = new Generator(inputDirectory, outputDirectory);
       generator.setViewsPackageName(viewsPackageName);
